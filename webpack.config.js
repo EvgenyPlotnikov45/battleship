b8e1375cc5986a80f27bb509cbd88303d18ac62a
EvgenyPlotnikov45 <dlccc4596@yandex.ru>
2018-03-07 13:52:03 +0500

    add babel and test ES6

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,42 @@
 'use strict';
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/app.js',
+    mode: 'development',
+    entry: 'app',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    devtool: 'source-map',
+    watch: true,
+    watchOptions: {
+     aggregateTimeout: 100
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                    // plugins: ['@babel/transform-runtime']
+                }
+            }
+        }
+        ]
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, "src"), "node_modules"]
+    },
+    plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        title: 'BattleShip'
+    })
+    ]
 };
